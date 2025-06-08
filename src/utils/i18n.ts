9d5392afa160b49dac706dd20d3c9229ceb04d64
Leon Dore <leon.dore@gmail.com>
2025-06-08 10:52:53 -0400

    translate description and add og logo

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -7,6 +7,8 @@ export const defaultLang = "es";
 
 export const ui = {
   es: {
+    "head.description":
+      "No leemos el cielo para huir de la tierra, sino para habitarla mas profundamente. Adela Dore, artista y astróloga afrocaribeña, nacida y residente en Ayiti.",
     "nav.menu": "Menu",
     "nav.about": "Quien Soy",
     "nav.services": "Servicios",
@@ -16,6 +18,8 @@ export const ui = {
     "footer.follow": "Sígueme:",
   },
   en: {
+    "head.description":
+      "We interpret the stars not to escape the Earth, but to inhabit it more deeply. Adela Dore, Afro-Caribbean artist and astrologer, born and based on the island of Ayiti.",
     "nav.menu": "Menu",
     "nav.about": "About",
     "nav.services": "Services",
